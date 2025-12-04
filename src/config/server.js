29633e073ea8d74f7f0cd30740ff9142e8f16dd3
MiguelP099 <miguel.prieto099@gmail.com>
2025-12-04 18:33:49 -0500

    Adding temporary changes to Github

--- src/config/server.js
+++ src/config/server.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const initServer = () => {
     const app = express();
+    app.use((req, res, next) => {
+  console.log("Request received:", req.method, req.url);
+  next();
+});
     app.use(cors());
     app.use(bodyParser.json());
     return app;
